da657eb9624e3b3ed5c22f035c3044356f0db9fb
Seu Nome <edilsonlm217@hotmail.com>
2025-03-25 21:47:32 -0400

    refactor: improved readability

--- src/core/whatsapp/session-manager/whatsapp-session.ts
+++ src/core/whatsapp/session-manager/whatsapp-session.ts
@@ -7,76 +7,59 @@ export class WhatsAppSession {
   private socket: WASocket | null = null;
   private sessionEvents = new Subject<{ type: string; data?: any }>();
   private baileysEvents = new Subject<{ type: string; data?: any }>();
-  // Adicionamos esta lista para controlar os eventos aos quais estamos assinando
-  private subscribedEvents: (keyof BaileysEventMap)[] = [];
-
-  // Armazena as meta informações
-  private metaInfo: { [key: string]: any } = {};
+  private subscribedEvents: (keyof BaileysEventMap)[] = []; // Adicionamos esta lista para controlar os eventos aos quais estamos assinando
+  private metaInfo: { [key: string]: any } = {}; // Armazena as meta informações
 
   constructor(private sessionId: string, private authService: AuthStateService) { }
-
-  get sessionEvents$() {
-    return this.sessionEvents.asObservable();
-  }
-
-  get baileysEvents$() {
-    return this.baileysEvents.asObservable();
-  }
 
   async iniciarSessao() {
     this.emitEvent('starting');
     await this.setupSocket();
   }
 
-  async reconectarSessao() {
-    this.emitEvent('reconnecting');
-    await this.setupSocket();
+  async desconectar() {
+    if (this.socket) { await this.socket.logout() }
   }
 
   private async setupSocket() {
     const state = await this.authService.getAuthState(this.sessionId);
-    this.socket = makeWASocket({
-      printQRInTerminal: true,
-      auth: state,
-      qrTimeout: 20000,
-    });
 
-    this.socket.ev.on('creds.update', async () => {
-      await this.authService.saveCreds(this.sessionId, state.creds);
+    this.socket = makeWASocket({ printQRInTerminal: true, auth: state, qrTimeout: 20000 });
+    this.socket.ev.on('creds.update', async () => await this.onCredsUpdate());
+    this.socket.ev.on('connection.update', async (update) => await this.onConnectionUpdate(update));
 
-      // Armazena as informações nas meta informações
-      const authState = this.socket?.authState;
-      this.setMetaInfo('phone', authState?.creds?.me?.id);
-      this.setMetaInfo('phonePlatform', authState?.creds?.platform);
-
-      this.baileysEvents.next({ type: 'creds.update', data: this.metaInfo });
-    });
-
-    this.socket.ev.on('connection.update', async (update) => {
-      this.setMetaInfo('connectionState', update);
-      this.baileysEvents.next({ type: 'connection.update', data: this.metaInfo });
-
-      if (update.qr) this.onQRCodeReceived(update.qr);
-      if (update.connection === 'open') await this.onSessionOpened();
-      if (update.connection === 'close') await this.onSessionClosed(update);
-    });
-
-    // Aqui, inscrevemos os eventos que que assinamos
-    this.subscribedEvents.push('connection.update', 'creds.update');
-  }
-
-  // Método para adicionar ou atualizar meta informação
-  setMetaInfo(key: string, value: any) {
-    this.metaInfo[key] = value;
-  }
-
-  // Método para acessar meta informação
-  getMetaInfo(key: string) {
-    return this.metaInfo[key];
+    this.subscribedEvents.push('connection.update', 'creds.update'); // Aqui, inscrevemos os eventos que que assinamos
   }
 
   private onQRCodeReceived(qr: string) {
     this.emitEvent('qr_code', qr);
+  }
+
+  // Novo método separado para o callback
+  private async onCredsUpdate() {
+    const state = await this.authService.getAuthState(this.sessionId);
+
+    // Salva as credenciais atualizadas
+    await this.authService.saveCreds(this.sessionId, state.creds);
+    const authState = this.socket?.authState;
+    this.setMetaInfo('phone', authState?.creds?.me?.id);
+    this.setMetaInfo('phonePlatform', authState?.creds?.platform);
+
+    // Emite o evento 'creds.update' com as meta informações
+    this.baileysEvents.next({ type: 'creds.update', data: this.metaInfo });
+  }
+
+  // Novo método separado para o callback
+  private async onConnectionUpdate(update: Partial<ConnectionState>) {
+    // Atualiza o estado da conexão nas meta informações
+    this.setMetaInfo('connectionState', update);
+
+    // Emite o evento 'connection.update' com as meta informações
+    this.baileysEvents.next({ type: 'connection.update', data: this.metaInfo });
+
+    if (update.qr) this.onQRCodeReceived(update.qr);
+    if (update.connection === 'open') await this.onSessionOpened();
+    if (update.connection === 'close') await this.onSessionClosed(update);
   }
 
   private async onSessionOpened() {
@@ -97,8 +80,14 @@ export class WhatsAppSession {
     }
   }
 
-  async desconectar() {
-    if (this.socket) { await this.socket.logout() }
+  // Método para adicionar ou atualizar meta informação
+  private setMetaInfo(key: string, value: any) {
+    this.metaInfo[key] = value;
+  }
+
+  private async reconectarSessao() {
+    this.emitEvent('reconnecting');
+    await this.setupSocket();
   }
 
   private async limparSessao() {
@@ -122,5 +111,13 @@ export class WhatsAppSession {
 
   private emitEvent(type: string, data?: any) {
     this.sessionEvents.next({ type, data });
+  }
+
+  get sessionEvents$() {
+    return this.sessionEvents.asObservable();
+  }
+
+  get baileysEvents$() {
+    return this.baileysEvents.asObservable();
   }
 }
